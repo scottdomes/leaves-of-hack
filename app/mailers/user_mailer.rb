dbdcebe76afc8af20e171927b6d128ffc28e7354
scottdomes <scottdomes@gmail.com>
2016-03-29 16:31:10 -0700

    Set up mailer and sample email message

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+  def deletion_email
+    @user = user
+    @url = "http://rottenmangos.com"
+    mail(to: @user.email, subject: "You got ROCKED (Deletion warning)")
+  end
+end
