6a4ac0f692eb60cf39b7131d07c047ef84ea2322
scottdomes <scottdomes@gmail.com>
2016-03-29 11:20:37 -0700

    Added image uploader

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,16 @@ class Movie < ActiveRecord::Base
 
   has_many :reviews
 
+  mount_uploader :image, ImageUploader
+
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validates :description, presence: true
   validates :poster_image_url, presence: true
   validates :release_date, presence: true
+  validates_processing_of :image
+  validate :image_size_validation
 
   validate :release_date_is_in_the_past
 
@@ -21,6 +25,11 @@ class Movie < ActiveRecord::Base
     if release_date.present?
       errors.add(:release_date, "should be in the past") if release_date > Date.today
     end
+  end
+
+  def image_size_validation 
+    if image.size > 0.5 megabytes
+      errors.add(:image, "should be less than 500kb")
   end
 
 end
