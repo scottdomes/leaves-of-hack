49dd75dd4a0b2985326c0beb1a5fc1f882512d03
scottdomes <scottdomes@gmail.com>
2016-03-29 11:50:05 -0700

    Added namespace access restrictions for non-admin users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,17 @@
 class Admin::UsersController < ApplicationController
 
+  before_filter :restrict_user_by_role
+
   def index
     @users = User.all
+  end
+
+  protected
+
+  def restrict_user_by_role
+    unless current_user.admin
+      flash[:notice] = "Get outta there! Not authorized."
+      redirect_to root_path
+    end
   end
 end
