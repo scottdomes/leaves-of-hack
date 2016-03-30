7211e6bc32eb43b67163594a01a5339cf20fcbcd
scottdomes <scottdomes@gmail.com>
2016-03-30 12:34:08 -0700

    Added link to user profile on review

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
 
   namespace :admin do
