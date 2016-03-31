b4a8cdab73ca614ac25765f2c59e93e23c82a40e
scottdomes <scottdomes@gmail.com>
2016-03-31 11:16:04 -0700

    Completed rename using gem

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'rename'
 
 gem 'carrierwave', '~> 0.10.0'
 gem 'mini_magick', '~> 4.3'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_rotten-mangos_session'
+Rails.application.config.session_store :cookie_store, key: '_leaves_of_hack_session'
